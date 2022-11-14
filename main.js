3bada462f3b67321c5ce07ad505da18d97b642bc
jeremyduc <duc.jeremy@sfr.fr>
2022-11-14 14:14:15 +0100

    finish

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
 const monBouton = document.querySelector("#fetchAll")
-const monBoutonSearch = document.querySelector("#triggerSearch")
+
 const countryContainer = document.querySelector(".lesPays")
+const langageCompteur = document.querySelector(".langageCompteur")
+
+const monBoutonSearch = document.querySelector("#triggerSearch")
 const searchField = document.querySelector("#search")
+
+const monBoutonLangage = document.querySelector("#triggerSearchLangage")
+const searchLangage = document.querySelector("#searchLangage")
 
 monBouton.addEventListener("click", ()=>{
     clearContainer()
@@ -11,6 +17,10 @@ monBouton.addEventListener("click", ()=>{
 monBoutonSearch.addEventListener("click", ()=>{
     clearContainer()
     fetchOne(searchField.value)
+})
+monBoutonLangage.addEventListener("click", ()=>{
+    clearContainer()
+    fetchLangage(searchLangage.value)
 })
 
 function fetchAll(){
@@ -49,7 +59,7 @@ function addCountryTemplate(country){
     let template = `
                 <div class="countryDiv">
                    
-                    <img height="20px" width="20px" src="${country.flags.png}" alt="">
+                    <img height="20px" width="30px" src="${country.flags.png}" alt="">
                     <p class="clickable">${country.name.common}</p>
                     
                 </div>
@@ -99,4 +109,28 @@ function addCountryPreciseTemplate(country){
 
 function clearContainer(){
     document.querySelector(".lesPays").innerHTML = ""
+    document.querySelector(".langageCompteur").innerHTML = ""
+}
+
+function fetchLangage(langage){
+    let url = `https://restcountries.com/v3.1/lang/${langage}`
+    fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            clearContainer()
+            let tableau = data
+            addNumberLangage(tableau)
+            tableau.forEach(country =>{
+                addCountryTemplate(country)
+            })
+        })
+}
+
+function addNumberLangage(langage){
+    let template = `
+                <div class="countryDiv">
+                    <p>${langage.length} countries talk this language</p>
+                </div>
+    `
+    langageCompteur.innerHTML += template
 }
